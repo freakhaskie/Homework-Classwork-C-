5f0f4c312672816ed0f24f82291c40e813eaa1c8
Sofiia Shestakova <80338515+freakhaskie@users.noreply.github.com>
2022-02-17 22:34:04 +0200

    Add files via upload

---
+++ shestakova_4.19(a).cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int n = 5;
+    float x = 2, s= 0;
+    for (int i = 1; i<=n; i++){
+        s = s + pow(x - 1, i);
+    }
+    s = s+1;
+    printf("Значення = %.2f\n", s);
+}
